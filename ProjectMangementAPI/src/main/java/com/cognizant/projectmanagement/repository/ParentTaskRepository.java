11bf46b0b69c2dd5b0ae2cd0990878c59241bff0
smanian <saranya.manian@cognizant.com>
2019-12-19 18:39:42 +0530

    Deleting files

--- ProjectMangementAPI/src/main/java/com/cognizant/projectmanagement/repository/ParentTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.projectmanagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cognizant.projectmanagement.dao.ParentTask;
-
-public interface ParentTaskRepository extends CrudRepository<ParentTask, Integer> {
-
-}
--- ProjectMangementAPI/src/main/java/com/cognizant/projectmanagement/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.projectmanagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cognizant.projectmanagement.dao.Project;
-
-public interface ProjectRepository extends CrudRepository<Project,Integer>{
-
-}
